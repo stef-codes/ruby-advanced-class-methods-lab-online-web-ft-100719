b6eb1f901e6b6beb5b60e5b61319f6381dc897af
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-20 01:59:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,79 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end 
+  
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    @@all << new_song
+    new_song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.each do |song|
+      if song.name == name 
+        return song 
+      end
+    end  
+    nil
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end 
+  end   
+  
+  def self.alphabetical
+    @@all.sort_by { |song| song.name  }
+  end 
+  
+  def self.new_from_filename(filename)
+    
+      # "Taylor Swift - Blank Space.mp3"
 
+         filename = filename.split(/\s*[-,.]\s*/)
+         artist_name = filename[0]
+         name = filename[1]
+      
+         song = self.new
+         song.name = name
+         song.artist_name = artist_name
+         song
+  end 
+  
+  def self.create_from_filename(filename)
+    
+         filename = filename.split(/\s*[-,.]\s*/)
+         artist_name = filename[0]
+         name = filename[1]
+         
+         # add file info to song class 
+         song = self.new
+         song.name = name
+         @@all << song
+         song.artist_name = artist_name
+         song
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  
+    
+  
 end
